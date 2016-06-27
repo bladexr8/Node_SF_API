f6cf19afec840e5f4a6db9c1fc258a22ec8d33c9
Stephen Moss <stephen.moss@cloudsherpas.com>
2016-06-27 22:02:44 +1000

    Added nforce-metadata plugin support

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // test app using nforce SF REST API Wrapper
 
+// nforce module reference
 var nforce = require('nforce');
+
+// nforce metadata plugin reference
+require('nforce-metadata')(nforce);
 
 // load credentials from external module
 // references credentials.js which contains SF credentials for org
 // source code package contains template of this file (credentials_copy.js)
 var credentials = require('./credentials.js');
+
+// underscore module reference
+var _ = require('underscore');
 
 // create SF connection
 console.log('***Creating Salesforce Connection...');
@@ -15,7 +22,11 @@ var org = nforce.createConnection({
 	redirectUri: 'https://localhost:3000/oauth/_callback',
 	apiVersion: '37.0',
 	environment: 'production',
-	mode: 'multi'
+	mode: 'multi',
+	metaOpts: { // options for nforce-metadata
+		interval: 2000
+	},
+	plugins: ['meta'] // loads the plugin in this connection
 });
 
 // multi user mode login
@@ -30,7 +41,28 @@ org.authenticate({
 		oauth = resp;
 		console.log('***Authentication Successful...');
 		console.log('***Access Token = ' + oauth.access_token);
+		printMetadata();
 	} else {
 		console.error('***Login Error Occurred: ' + err);
 	}
 });
+
+
+// print a list of metadata
+function printMetadata() {
+	console.log('***Retrieving List of Metadata...');
+	org.meta.listMetadata({
+		oauth: oauth,
+		queries: [
+			{ type: 'CustomObject' },
+			{ type: 'CustomField' },
+			{ type: 'ApexClass' }
+		]
+	}).then(function(meta) {
+		_.each(meta, function(r) {
+			console.log(r.type + ': ' + r.fullName + ' (' + r.fileName + ')');
+		});
+	}).error(function(err) {
+		console.error('***Error Retrieving Metadata - ' + err);
+	});
+}
